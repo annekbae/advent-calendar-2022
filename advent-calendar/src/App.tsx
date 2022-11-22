4c3019d56987d34afe8b9a78e26191b61e7e7c49
annekatb <akbakkelie@gmail.com>
2022-11-22 22:21:54 +0100

    added components folder

--- advent-calendar/src/App.tsx
+++ advent-calendar/src/App.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react'
 import { experimentalStyled as styled } from '@mui/material/styles'
-import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
-import Grid from '@mui/material/Unstable_Grid2'
 import './App.css'
 import { AppName } from './interfaces'
-import DoorGrid from './DoorGrid'
+import DoorGrid from './components/DoorGrid'
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
